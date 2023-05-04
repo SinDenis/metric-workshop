247be7d146280ed9e125d7356804e090802ad0ee
d.sin <d.sin@tinkoff.ru>
2023-05-04 16:35:51 +0300

    Init metric workshop

---
+++ src/main/java/com/example/metricworkshop/MetricWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.metricworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricWorkshopApplication.class, args);
+    }
+
+}
